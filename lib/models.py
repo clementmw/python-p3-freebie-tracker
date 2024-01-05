f592af77e7eab3a91a07948d23b05da7e379b3cd
Clement Macharia <clementmacharia62@gmail.com>
2024-01-05 13:26:05 +0300

    changes

--- lib/models.py
+++ lib/models.py
@@ -1,29 +1,59 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import create_engine
+from sqlalchemy import (Column,Integer,String,ForeignKey)
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
 
-Base = declarative_base(metadata=metadata)
+Base = declarative_base()
+engine  =  create_engine('sqlite:///freebies.db')
 
-class Company(Base):
+class Company:
     __tablename__ = 'companies'
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
-    founding_year = Column(Integer())
+    founding_year =Column(Integer())
 
+# represent data
     def __repr__(self):
-        return f'<Company {self.name}>'
+        return  f"company: {self.id}"\
+        f"name: {self.name}"\
+        f"founding year : {self.founding_year}"
+    
 
-class Dev(Base):
+# establish relationship to freeble
+    freebie = relationship('Freebie', backref= 'company')
+
+class Dev:
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name = Column(String())
 
+# represent data
     def __repr__(self):
-        return f'<Dev {self.name}>'
+        return f"dev {self.id}:"\
+        f"name:{self.name}"\
+        
+
+
+# establish relationship to freeble
+    dev =  relationship('Freebie', backref = 'dev')
+
+class Freebie:
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+
+# represent data
+    def __repr__(self):
+        return f"freebie {self.id}:"\
+        f"item name:{self.item_name}"\
+        f"value:{self.value}"
+
+
+# establish connection to both company and dev
+    company_id = Column(Integer(), ForeignKey('company_id'))
+    dev_id = Column(Integer(), ForeignKey('dev_id'))
